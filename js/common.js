0feff8fcae8355ded347b9ae8aeade2896f2ea10
VitaliyGluhenkiy <VitaliyGluhenkiy@gmail.com>
2019-01-08 20:53:59 +0200

    3re

--- js/common.js
+++ js/common.js
@@ -1,21 +1,62 @@
+function openNav() {
+	
+	if ($(".hamburger").hasClass("is-active")) {
+		$('.hamburger').removeClass('is-active');
+		$("#my-menu").css("width", "0");
+	}
+	else {
+		$("#my-menu").css("width","400px" );
+		$('.hamburger').addClass('is-active');
+	}
+}
+	
+	
+	
 $(function() {
 
-	$('#my-menu').mmenu({
-		extensions: [ 'widescreen' , 'effect-menu-slide' ],
-		navbar: {
-			title: '<img src="img/weblogo.jpg">'
-		},
-		offCanvas: {
-			position: 'right'
-		}
-	});
 
-	var api = $('#my-menu').data('mmenu');
-	api.bind('opened', function(){
-		$('.hamburger').addClass('is-active');
-	}).bind('closed', function(){
-		$('.hamburger').removeClass('is-active')
-	})
+	$(document).ready(function(){
+		// Add smooth scrolling to all links
+		$("a").on('click', function(event) {
+	  
+		  // Make sure this.hash has a value before overriding default behavior
+		  if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+	  
+			// Store hash
+			var hash = this.hash;
+	  
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+			  scrollTop: $(hash).offset().top
+			}, 1000, function(){
+		 
+			  // Add hash (#) to URL when done scrolling (default click behavior)
+			  window.location.hash = hash;
+			});
+		  } // End if
+		});
+	  });
+
+	// $('#my-menu').mmenu({
+	// 	extensions: [ 'widescreen' , 'effect-menu-slide' ],
+	// 	navbar: {
+	// 		title: '<img src="img/weblogo.jpg">'
+	// 	},
+	// 	offCanvas: {
+	// 		position: 'right'
+	// 	}
+	// });
+
+	// var api = $('#my-menu').data('mmenu');
+	// api.bind('opened', function(){
+	// 	$('.hamburger').addClass('is-active');
+	// }).bind('closed', function(){
+	// 	$('.hamburger').removeClass('is-active')
+	// })
+	
 
 	
 	$('.about-slide').owlCarousel({
